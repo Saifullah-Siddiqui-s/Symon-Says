e22368b6461df8b745dba6609f93272565d4c4a7
Saifullah Siddiqui <saifullahshasiddiqui@gmail.com>
2025-08-25 16:33:04 +0530

    Add files via upload

---
+++ symonsays/app.js
@@ -0,0 +1,83 @@
+let gameSeq = [];
+let userSeq = [];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener('keydown' , (event) => {
+    if(started == false) {
+       started = true;
+        
+       levelUp();
+    }
+});
+
+let start = document.querySelector('.restart')
+start.addEventListener('click' , (event) => {
+    if(started == false) {
+        started = true;
+        start.innerText = "Restart"
+        levelUp();
+    }
+    resetGame();
+});
+
+let btns = document.querySelectorAll('.btn');
+function levelUp() {
+    level++;
+    h2.innerText = `Level ${level}`;
+    let btn = Math.floor(Math.random() * 4);
+    gameSeq.push(btn);
+    gameFlash(btns[btn]);
+}
+
+function gameFlash(btn) {
+    btn.classList.add('flash');
+    setTimeout(() =>{
+        btn.classList.remove('flash')
+    }, 300);
+}
+
+
+function userFlash(btn) {
+    btn.classList.add('userflash');
+    setTimeout(() =>{
+        btn.classList.remove('userflash')
+    }, 300);
+}
+
+function btnPress(btn) {
+    userSeq.push(btn);
+    userFlash(btns[btn]);
+    if(checkAns()) {
+        setTimeout(() =>{
+            levelUp();
+            userSeq = [];  
+        }, 1000);
+    } else {
+        h2.innerText = `Wrong Sequence \n Your Highest Score is ${level-1}  \n Press any Key To Restart`;
+        resetGame();
+    }
+}
+
+for (let i = 0 ; i < btns.length ; i++) {
+    btns[i].addEventListener('click', (event) => {
+        btnPress(i);
+    })
+}
+
+function checkAns() {
+    return gameSeq.every((val,idx) => val === userSeq[idx]);
+}
+
+
+function resetGame() {
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
+
+
